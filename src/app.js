667139d0b32e2872b582964f4662acd43ffd5894
Юра <yura@MacBook-Pro-Ana.local>
2019-01-18 23:43:32 +0200

    Added slider and new-in sections

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import './sass/main.sass'
 import '@babel/polyfill'
+const Flickity = require('flickity')
+/* Slider */
+const slider = new Flickity('.slider__main', {
+    initialIndex: 0,
+    prevNextButtons: true,
+    freeScroll: false,
+    wrapAround: true,
+    autoPlay: 5000,
+    selectedAttraction: 0.05,
+    friction: 0.67
+})
